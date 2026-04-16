1c4a814347ab7f04fe52e8f77bbb257779721ce0
Dalton Chris <daltonchris2@gmail.com>
2026-04-16 15:12:48 +0930

    Reapply "Conflict fix"
    This reverts commit 89eb4ec3544bfc822162e87f9d7faad479844033.

--- Source/PPR301_FPS/WaveManager.h
+++ Source/PPR301_FPS/WaveManager.h
@@ -3,6 +3,8 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "WaveUI.h"
+#include "Sound/SoundBase.h"     
+#include "Kismet/GameplayStatics.h"
 #include "TDSPlayerController.h"
 #include "WaveManager.generated.h"
 
@@ -35,6 +37,9 @@ class PPR301_FPS_API AWaveManager : public AActor
 
 public:
     AWaveManager();
+    /** Sound that plays when a new wave starts */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+    USoundBase* WaveStartSound = nullptr;
 
 protected:
     virtual void BeginPlay() override;
@@ -45,6 +50,7 @@ protected:
     void HandleNextWave();
     void AddCashForWaveComplete();
     void GiveKillReward();
+    void PlayWaveStartSound();
 
 public:
 
